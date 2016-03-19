42536b819af711fc1f7f44650f62e5f389272b71
Fredrik Rubensson <fredrik@eldfluga.se>
2016-03-19 11:40:31 +0100

    clock ticking

--- pomotray.rb
+++ pomotray.rb
@@ -7,10 +7,25 @@ application = Qt::Application.new ARGV
 
 
 class Pomotray < Qt::SystemTrayIcon
+  slots :tick
+  
   def initialize
     super tomato
     self.context_menu = menu
+    @timer = Qt::Timer.new
+    @timer.set_interval 1000
+
+    self.connect @timer, SIGNAL( :timeout ), self, SLOT( :tick )
+    
+    @seconds_elapsed = 0
+    @count_down_minutes = 25
+    update_minutes
+    @timer.start
     show
+  end
+
+  def update_minutes
+    self.set_tool_tip "#{seconds_left/60}:#{seconds_left%60}"
   end
 
   def tomato
@@ -26,11 +41,18 @@ class Pomotray < Qt::SystemTrayIcon
       menu.add_action 'pause'
     end
   end
+
+  def seconds_left
+    @count_down_minutes * 60 - @seconds_elapsed
+  end
+
+  def tick
+    @seconds_elapsed += 1
+    update_minutes
+  end
 end
 
-
 pomotray = Pomotray.new
-#pomotray.show
 
 application.exec
 
