8a113ab67a5f3d94cc8a86cfdf5fa4a6ccbf5a33
Fredrik Rubensson <fredrik@eldfluga.se>
2016-03-19 11:13:43 +0100

    menu on right click

--- pomotray.rb
+++ pomotray.rb
@@ -5,8 +5,24 @@ require 'Qt'
 Qt::Application::set_style 'plastique'
 application = Qt::Application.new ARGV
 
-tomato = Qt::Icon.new 'tomato.png'
-Qt::SystemTrayIcon.new( tomato, application ).show
+# need a window to make the menu happy
+window = Qt::MainWindow.new
+
+def tomato
+  Qt::Icon.new 'tomato.png'
+end
+
+def tomato_menu window
+  Qt::Menu.new( window ) do |menu|
+    menu.add_action 'start'
+    menu.add_action 'stop'
+    menu.add_action 'pause'
+  end
+end
+
+pomotray = Qt::SystemTrayIcon.new tomato
+pomotray.context_menu = tomato_menu window
+pomotray.show
 
 application.exec
 
